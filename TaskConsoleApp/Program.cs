76fa7e2197988c679cf0566f3f3b460a3e085df1
Kağan <kagandoganx@gmail.com>
2024-08-06 00:09:16 +0300

    Result and ValueTask

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -26,13 +26,11 @@ namespace TaskConsoleApp
 
     internal class Program
     {
-
+        public static int cachedata { get; set; } = 450;
         public static string CacheData { get; set; }
         public async static Task Main(string[] args)
         {
             
-
-
             Console.Out.WriteLine("Main Thread: " + Thread.CurrentThread.ManagedThreadId);
 
             List<string> urlsList = new List<string>()
@@ -47,14 +45,29 @@ namespace TaskConsoleApp
 
 
 
-            List<Task<Content>> taskList = new List<Task<Content>>();
+            //List<Task<Content>> taskList = new List<Task<Content>>();
 
-            urlsList.ToList().ForEach(x =>
-            {
-                taskList.Add(GetContentAsync(x));
-            });
+            //urlsList.ToList().ForEach(x =>
+            //{
+            //    taskList.Add(GetContentAsync(x));
+            //});
 
 
+            #region ValueTask
+            // await GetDataForValueTask(); // ValueTask => Normal Task bir classtır. Class demek referans tip demek.
+                                         // Adamlar demişki biz en ufak şeylerde referans tip olan Task dönmek yerine aynısının Struct yapısını yapmışlar yani ValueTask.
+                                         // O yüzden memory'yi çok meşgul etmemek için, çok hızlı gelen datalar için Task kullanmaıp ValueTask kulanabiliriz.
+                                         // Mesela cachelenmiş bir data için kullanılabilir. Memoryde hazır ve bizi bekliyor çünkü o data
+            #endregion
+
+
+            #region Result
+            // Console.WriteLine(GetData()); // Result propertysi => Asenkron metot .ağrımı yaptığımızda bize geriye yenibir nesne örneği dönüyordu.
+            // bu nesnenin içinde result prop'u var. bu prop ile beraber dönen datayı alabiliriz. Fakat bu Result prop'u o an ki UI Thread i bloklar.
+            // peki neden buna ihtiyacımız var? normal bir metot üzerinden async bir metot çağıracağım ama geriye task dönmüyorum. bu durumlarda kullanılır.
+            // ve bu prop u datanın geldiğindne eminsem de kullanabilirim.
+            // Bundan nasıl emin olabilirim peki? önceden await ile değişkende çağırıp ardından o değişkene .Result yaparım. ama ilgili metodun async olması gerek await atıyorum çünkü
+            #endregion
 
             #region FromResult 
             // parametre olarak bir obje alır. bu obje değerini geriye bir task nesne örneği ile beraber dönmektedir.
@@ -157,6 +170,18 @@ namespace TaskConsoleApp
             {
                 return Task.FromResult<string>(CacheData);
             }
+        }
+
+        public static string GetData() // Normal bir metpt içerisine async birmetot çağrımıyaptım
+        {
+            var task = new HttpClient().GetStringAsync("https://www.google.com");
+
+            return task.Result; // bu arkadaş UI thread i blokladı.
+        }
+        public static ValueTask<int> GetDataForValueTask() 
+        {
+
+            return new ValueTask<int>(cachedata);
         }
     }
 }
